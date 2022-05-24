18be86d43871d2738eaf9e44b4ffe205c9df3936
Abdul Muheet Ghani <81433387+Abdul-muheet-ghani@users.noreply.github.com>
2022-05-24 12:38:37 +0500

    Update source_file.sh

--- source_file.sh
+++ source_file.sh
@@ -2,7 +2,7 @@ cd aws-fpga
 source hdk_setup.sh
 cd ../scalar-unit
 source setup.bash
-cd ../New_Folder/verif/scripts/
+cd ../CL/verif/scripts/
 make TEST=test_ddr
 cd ../sim/vivado/test_ddr
 xsim -gui tb
