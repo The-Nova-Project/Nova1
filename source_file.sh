0728c4600185bbec62c54e0ba4932df6d9e9727c
Abdul-muheet-ghani <aghani@students.uit.edu>
2022-08-19 20:04:09 +0000

    Add synthesis and source file

--- source_file.sh
+++ source_file.sh
@@ -2,7 +2,10 @@ cd aws-fpga
 source hdk_setup.sh
 cd ../scalar-unit
 source setup.bash
-cd ../CL/verif/scripts/
-make TEST=test_ddr
-cd ../sim/vivado/test_ddr
+rm -rf /home/$USER/update_bd/afi_check/aws-fpga/hdk/cl/developer_designs/nova_project/verif/sim/
+cd ../aws-fpga/hdk/cl/developer_designs/nova_project/
+export CL_DIR=$(pwd)
+cd verif/scripts/
+make TEST=test_ddr_peek_poke
+cd ../sim/vivado/test_ddr_peek_poke_sv/
 xsim -gui tb
